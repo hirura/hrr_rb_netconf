67fbb1b5390ce34a9a323aa70e669224b2954a35
hirura <hirura@gmail.com>
2019-06-02 19:16:52 +0900

    fix confirmed-commit 1.0 capability with deleting 1.1 features

--- lib/hrr_rb_netconf/server/capability/confirmed_commit_1_0.rb
+++ lib/hrr_rb_netconf/server/capability/confirmed_commit_1_0.rb
@@ -9,15 +9,8 @@ module HrrRbNetconf
         DEPENDENCIES = ['urn:ietf:params:netconf:capability:candidate:1.0']
         IF_FEATURES  = ['candidate', 'confirmed-commit']
 
-        oper_proc('cancel-commit'){ |session, datastore, input_e|
-          datastore.run 'cancel-commit', input_e
-          '<ok />'
-        }
-
         model 'commit', ['confirmed'],       'leaf', 'type' => 'empty'
         model 'commit', ['confirm-timeout'], 'leaf', 'type' => 'integer', 'range' => [1, 2**32-1], 'default' => '600'
-        model 'commit', ['persist'],         'leaf', 'type' => 'string'
-        model 'commit', ['persist-id'],      'leaf', 'type' => 'string'
       end
     end
   end
